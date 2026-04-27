a6a3e3137eb94043aa26b00d0eaf34a5d5d5265b
Wycliffe Wasonga <wycliffewasonga46@gmail.com>
2026-04-27 20:04:00 +0300

    Create day3_part1.py

---
+++ day3_part1.py
@@ -0,0 +1,12 @@
+# Day 3 Python Practice - Functions
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+print(multiply(6, 2))
+print(divide(10, 2))
